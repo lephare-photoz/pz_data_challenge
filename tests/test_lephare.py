f368d2cd11c35de0ec30b345779b9ae1eab77603
Raphael Shirley <rshirley@mpe.mpg.de>
2026-06-26 15:45:28 +0200

    Tests 3 and 4 passing

--- tests/test_lephare.py
+++ tests/test_lephare.py
@@ -16,6 +16,8 @@ import lephare as lp
 
 from pz_data_challenge.taskset_1 import run_taskset_1
 from pz_data_challenge.taskset_2 import run_taskset_2
+from pz_data_challenge.taskset_3 import run_taskset_3
+from pz_data_challenge.taskset_4 import run_taskset_4
 
 from pz_data_challenge import submit_utils
 
@@ -295,6 +297,216 @@ def run_taskset_2_training_and_estimation(
     pz_out.write()
 
 
+def run_taskset_3_estimation_only(
+    model_file: str | Path,
+    test_file: str | Path,
+    output_file: str | Path,
+) -> None:
+    """
+    User supplied function to run estimation for task set 1
+
+    This function should use a model stored in model_file, which
+    is downloaded as part of the submission tar file.
+
+    This function should write output data to output_file in qp
+    format.
+
+    Parameters
+    ----------
+    model_file:
+        Path to the model.  This should be part of the submission
+        tar file.
+    test_file:
+        Path to the test file contains the photometric test data on
+        which the PZ estimation will be run
+    output_file:
+        Path to write the output data to.  The output data should
+        be written in qp format.
+    """
+    test_data = TableHandle("test", path=test_file)
+    estimator = LephareEstimator.make_stage(
+        name="estimate_lephare",
+        model=model_file,
+        output_mode="return",
+        run_dir=SUBMIT_DIR,
+        bands=flux_cols,
+        err_bands=flux_err_cols,
+        hdf5_groupname="",
+        lephare_config_from_model=False,
+        **params,
+    )
+    pz_out = estimator.estimate(test_data)
+    pz_out.data.ancil["object_id"] = test_data()["object_id"].astype(int)
+    pz_out.path = output_file
+    pz_out.write()
+
+
+def run_taskset_3_training_and_estimation(
+    train_file: str | Path,
+    test_file: str | Path,
+    output_file: str | Path,
+) -> None:
+    """
+    User supplied function to run training and estimation for task set 1
+
+    This function should train a model and use it.
+
+    This function should write output data to output_file in qp
+    format.
+
+    Parameters
+    ----------
+    train_file:
+        Path to the test file contains the photometric test data on
+        which the PZ estimation will be trained
+    test_file:
+        Path to the test file contains the photometric test data on
+        which the PZ estimation will be run
+    output_file:
+        Path to write the output data to.  The output data should
+        be written in qp format.
+    """
+    train_data = TableHandle("train", path=train_file)
+    test_data = TableHandle("test", path=test_file)
+
+    informer = LephareInformer.make_stage(
+        name="inform_lephare",
+        nondetect_val=np.nan,
+        model="lephare.pkl",
+        hdf5_groupname="",
+        **params,
+        bands=flux_cols,
+        err_bands=flux_err_cols,
+        ref_band="mag_g_lsst",
+        zmin=float(config["Z_STEP"].split(",")[1]),
+        zmax=float(config["Z_STEP"].split(",")[2]),
+        nzbins=1
+        + float(config["Z_STEP"].split(",")[2]) / float(config["Z_STEP"].split(",")[0]),
+    )
+    model = informer.inform(train_data)
+
+    estimator = LephareEstimator.make_stage(
+        name="estimate_lephare",
+        # nondetect_val=np.nan,
+        model="lephare.pkl",
+        hdf5_groupname="",
+        # aliases=dict(input="test_data", output="lephare_estim"),
+        # use_inform_offsets=False,
+        bands=flux_cols,
+        err_bands=flux_err_cols,
+        output_mode="return",
+    )
+    pz_out = estimator.estimate(test_data)
+    pz_out.data.ancil["object_id"] = test_data()["object_id"].astype(int)
+    pz_out.path = output_file
+    pz_out.write()
+
+
+def run_taskset_4_estimation_only(
+    model_file: str | Path,
+    test_file: str | Path,
+    output_file: str | Path,
+) -> None:
+    """
+    User supplied function to run estimation for task set 1
+
+    This function should use a model stored in model_file, which
+    is downloaded as part of the submission tar file.
+
+    This function should write output data to output_file in qp
+    format.
+
+    Parameters
+    ----------
+    model_file:
+        Path to the model.  This should be part of the submission
+        tar file.
+    test_file:
+        Path to the test file contains the photometric test data on
+        which the PZ estimation will be run
+    output_file:
+        Path to write the output data to.  The output data should
+        be written in qp format.
+    """
+    test_data = TableHandle("test", path=test_file)
+    estimator = LephareEstimator.make_stage(
+        name="estimate_lephare",
+        model=model_file,
+        output_mode="return",
+        run_dir=SUBMIT_DIR,
+        bands=flux_cols,
+        err_bands=flux_err_cols,
+        hdf5_groupname="",
+        lephare_config_from_model=False,
+        **params,
+    )
+    pz_out = estimator.estimate(test_data)
+    pz_out.data.ancil["object_id"] = test_data()["object_id"].astype(int)
+    pz_out.path = output_file
+    pz_out.write()
+
+
+def run_taskset_4_training_and_estimation(
+    train_file: str | Path,
+    test_file: str | Path,
+    output_file: str | Path,
+) -> None:
+    """
+    User supplied function to run training and estimation for task set 1
+
+    This function should train a model and use it.
+
+    This function should write output data to output_file in qp
+    format.
+
+    Parameters
+    ----------
+    train_file:
+        Path to the test file contains the photometric test data on
+        which the PZ estimation will be trained
+    test_file:
+        Path to the test file contains the photometric test data on
+        which the PZ estimation will be run
+    output_file:
+        Path to write the output data to.  The output data should
+        be written in qp format.
+    """
+    train_data = TableHandle("train", path=train_file)
+    test_data = TableHandle("test", path=test_file)
+
+    informer = LephareInformer.make_stage(
+        name="inform_lephare",
+        nondetect_val=np.nan,
+        model="lephare.pkl",
+        hdf5_groupname="",
+        **params,
+        bands=flux_cols,
+        err_bands=flux_err_cols,
+        ref_band="mag_g_lsst",
+        zmin=float(config["Z_STEP"].split(",")[1]),
+        zmax=float(config["Z_STEP"].split(",")[2]),
+        nzbins=1
+        + float(config["Z_STEP"].split(",")[2]) / float(config["Z_STEP"].split(",")[0]),
+    )
+    model = informer.inform(train_data)
+
+    estimator = LephareEstimator.make_stage(
+        name="estimate_lephare",
+        # nondetect_val=np.nan,
+        model="lephare.pkl",
+        hdf5_groupname="",
+        # aliases=dict(input="test_data", output="lephare_estim"),
+        # use_inform_offsets=False,
+        bands=flux_cols,
+        err_bands=flux_err_cols,
+        output_mode="return",
+    )
+    pz_out = estimator.estimate(test_data)
+    pz_out.data.ancil["object_id"] = test_data()["object_id"].astype(int)
+    pz_out.path = output_file
+    pz_out.write()
+
+
 def test_example_taskset_1(
     setup_public_area: int,
     setup_submit_area: int,
@@ -334,4 +546,46 @@ def test_example_taskset_2(
         SUBMISSION_NAME,
         run_taskset_2_estimation_only,
         run_taskset_2_training_and_estimation,
+    )
+
+
+def test_example_taskset_3(
+    setup_public_area: int,
+    setup_submit_area: int,
+) -> None:
+    """
+    Test fuction to validate a submisson for Taskset 3
+
+    You should not need to change this function
+    """
+
+    assert setup_public_area == 0
+    assert setup_submit_area == 0
+
+    run_taskset_3(
+        PUBLIC_AREA,
+        SUBMISSION_NAME,
+        run_taskset_3_estimation_only,
+        run_taskset_3_training_and_estimation,
+    )
+
+
+def test_example_taskset_4(
+    setup_public_area: int,
+    setup_submit_area: int,
+) -> None:
+    """
+    Test fuction to validate a submisson for Taskset 4
+
+    You should not need to change this function
+    """
+
+    assert setup_public_area == 0
+    assert setup_submit_area == 0
+
+    run_taskset_4(
+        PUBLIC_AREA,
+        SUBMISSION_NAME,
+        run_taskset_4_estimation_only,
+        run_taskset_4_training_and_estimation,
     )
